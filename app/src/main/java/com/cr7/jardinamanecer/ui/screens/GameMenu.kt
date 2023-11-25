f370bb69bc18310808c9a336deaad782b112c906
karlajacome08 <88672417+karlajacome08@users.noreply.github.com>
2023-11-25 02:19:26 -0600

    Login Admin y Alumno (#11)
    * Login Admin y Alumno
    
    * Update GameMenu.kt
    
    Inclui flecha de regreso desde Game Menu
    
    * update con fondo y tamanos
    
    ---------
    
    Co-authored-by: Santiago Quihui <39177932+kiwis08@users.noreply.github.com>

--- app/src/main/java/com/cr7/jardinamanecer/ui/screens/GameMenu.kt
+++ app/src/main/java/com/cr7/jardinamanecer/ui/screens/GameMenu.kt
@@ -2,16 +2,34 @@ package com.cr7.jardinamanecer.ui.screens
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.cr7.jardinamanecer.R
 import com.cr7.jardinamanecer.navigation.Screens
@@ -38,10 +56,9 @@ fun GameMenu(navController: NavController) {
 
 
 
-    LazyRow(
-        modifier = Modifier.fillMaxSize(),
-        state = rememberLazyListState(),
-        contentPadding = PaddingValues(horizontal = 5.dp, vertical = 0.dp)
+    Box(modifier = Modifier
+        .paint(painterResource(id = R.drawable.menuscreen),
+            contentScale = ContentScale.FillBounds)
     ) {
         items(gamelists.size) { page ->
             Box(
@@ -58,12 +75,7 @@ fun GameMenu(navController: NavController) {
                             }
 
                             2 -> {
-                                try {
-                                    navController.navigate(Screens.Level1Game3.route)
-                                } catch (e: Exception) {
-                                    println("No funciono: ${e.message}")
-
-                                }
+                                navController.navigate(Screens.Level1Game3.route)
                             }
 
                             3 -> {
@@ -101,21 +113,6 @@ fun GameMenu(navController: NavController) {
                             11 -> {
                                 navController.navigate(Screens.Level4Game3.route)
                             }
-
-                            3 -> {
-                                println("Click en card de pagina $page")
-                            }
-
-                            4 -> {
-                                println("Juego 4")
-                            }
-
-                            5 -> {
-                                navController.navigate(Screens.Level4Game1.route)
-                                println("Juego 4")
-                            }
-
-
                         }
                     }
             ) {
@@ -126,6 +123,33 @@ fun GameMenu(navController: NavController) {
                     )
             }
         }
+
+
+        Box(
+            modifier = Modifier
+                .offset(x = 80.dp, y = 50.dp)
+        ) {
+        Image(
+            painter = painterResource(id = R.drawable.regresar),
+            contentDescription = null,
+            modifier = Modifier
+                .size(45.dp)
+
+                .clickable {
+                    navController.navigate(Screens.Start.route)
+                }
+        )
+        Text(
+            style = MaterialTheme.typography.displayLarge.copy(
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            ),
+            fontSize = 40.sp,
+            modifier = Modifier
+                .offset(x = 60.dp, y = -5.dp),
+            text = "Hola Santiago!"
+        )
+    }
     }
 
 }
