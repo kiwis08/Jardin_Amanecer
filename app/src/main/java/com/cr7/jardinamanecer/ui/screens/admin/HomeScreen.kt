e44f206bf433472467bf590a1ea7c8d0bfaf7585
karlajacome08 <a00833141@itesm.mx>
2023-11-28 23:17:07 -0600

    Update HomeScreen.kt

--- app/src/main/java/com/cr7/jardinamanecer/ui/screens/admin/HomeScreen.kt
+++ app/src/main/java/com/cr7/jardinamanecer/ui/screens/admin/HomeScreen.kt
@@ -34,29 +34,15 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.cr7.jardinamanecer.R
-import com.cr7.jardinamanecer.navigation.Screens
-import kotlinx.coroutines.launch
+import com.cr7.jardinamanecer.R import kotlinx.coroutines.launch
 
 
 @OptIn(ExperimentalMaterial3Api::class)
